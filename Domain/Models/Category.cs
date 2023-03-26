2118af4a305d4544d0e8b669ed7ff2e7ce2e5d2d
kleymenovaTatiana <124988961+kleymenovaTatiana@users.noreply.github.com>
2023-03-27 01:32:10 +0300

    Domain

---
+++ Domain/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.Models 
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products1s = new HashSet<Products1>();
+        }
+
+        public int CategoryId { get; set; }
+        public string ForDogs { get; set; } = null!;
+        public string ForCats { get; set; } = null!;
+        public string Aquariums { get; set; } = null!;
+        public string ForBirds { get; set; } = null!;
+
+        public virtual Filter? Filter { get; set; }
+        public virtual ICollection<Products1> Products1s { get; set; }
+    }
+}
