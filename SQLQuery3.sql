455de4b2d8c61f76aef981d2fab6e7c4472718a4
kleymenovaTatiana <124988961+kleymenovaTatiana@users.noreply.github.com>
2023-03-31 14:35:14 +0300

    insert into Customers

---
+++ SQLQuery3.sql
@@ -0,0 +1,16 @@
+insert into Customers
+(
+ClieNt_code,
+Nickname,
+Password,
+Surname,
+Name,
+Middle_name,
+Mail,
+Phone_number,
+Birthdate
+)
+values
+('1','Sun','12345','Yakovleva','Olga','Petrovna','aquapark@mail.ru','78066133','2006-09-20'),
+('2','Cat','54321','Zhuravleva','Evgeniya','Glebovna','dog@mail.ru','75003718','1990-05-23'),
+('3','Fish','78945','Maximov','Timofey','Romanovich','car@mail.ru','76880794','1993-02-02')
