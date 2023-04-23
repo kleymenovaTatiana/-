40960b7636b74fd0af2edbe687676549b52e7d5d
kleymenovaTatiana <124988961+kleymenovaTatiana@users.noreply.github.com>
2023-04-23 23:29:50 +0300

    Add files via upload

---
+++ WebApplication по частям/Contracts/Filter/CreateFilterRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Contracts.Filter
+{
+    public class CreateFilterRequest
+    {
+        public string Feed { get; set; } = null!;
+        public string Toys { get; set; } = null!;
+        public string Bowls { get; set; } = null!;
+        public string Aquariums { get; set; } = null!;
+    }
+}
