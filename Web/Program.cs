5ed260f38d6b2b5b97ccf3bd508a6a708220335b
kleymenovaTatiana <124988961+kleymenovaTatiana@users.noreply.github.com>
2023-05-26 06:48:06 +0300

    Task11

--- Web/Program.cs
+++ Web/Program.cs
@@ -3,8 +3,11 @@ using DataAccess.Wrapper;
 using Domain.Interfaces;
 using Domain.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Web.Auth;
 using Web.Data;
 
 namespace Web
@@ -29,9 +32,14 @@ namespace Web
             builder.Services.AddScoped<IStaffrService, StaffrService>();
 
             // Add services to the container.
+            builder.Services.AddAuthenticationCore();
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            builder.Services.AddScoped<ProtectedSessionStorage>();
+            builder.Services.AddScoped<ProtectedLocalStorage>();
 
             var app = builder.Build();
 
