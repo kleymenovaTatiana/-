e32eae84ad660c5a91b0dc31d14211dd18923de0
kleymenovaTatiana <124988961+kleymenovaTatiana@users.noreply.github.com>
2023-05-26 06:47:18 +0300

    Task11

---
+++ Web/Auth/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using System.Security.Claims;
+
+namespace Web.Auth
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ProtectedSessionStorage _sessionStorage;
+        private readonly ProtectedLocalStorage _localStorage;
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public CustomAuthenticationStateProvider(ProtectedSessionStorage sessionStorage, ProtectedLocalStorage localStorage)
+        {
+            _sessionStorage = sessionStorage;
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                var userSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
+                var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
+
+                if (userSession == null)
+                {
+                    return await Task.FromResult(new AuthenticationState(_anonymous));
+                }
+
+                var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.Sid, userSession.ClieNtCode),
+                new Claim(ClaimTypes.Name, userSession.Nickname),
+                new Claim(ClaimTypes.Email, userSession.Mail),
+                new Claim(ClaimTypes.Role, userSession.Role)
+            }, "CustomAuth"));
+
+                return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+            }
+            catch
+            {
+                return await Task.FromResult(new AuthenticationState(_anonymous));
+            }
+        }
+
+        public async Task UpdateAuthenticationStateAsync(UserSession userSession)
+        {
+            ClaimsPrincipal claimsPrincipal;
+
+            if (userSession != null)
+            {
+                await _sessionStorage.SetAsync("UserSession", userSession);
+                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                {
+                    new Claim(ClaimTypes.Sid, userSession.ClieNtCode),
+                    new Claim(ClaimTypes.Name, userSession.Nickname), 
+                    new Claim(ClaimTypes.Email, userSession.Mail),
+                    new Claim(ClaimTypes.Role, userSession.Role)
+                }));
+            }
+            else
+            {
+                await _sessionStorage.DeleteAsync("UserSession");
+                claimsPrincipal = _anonymous;
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+        }
+    }  
+}
---
+++ Web/Auth/UserSession.cs
@@ -0,0 +1,11 @@
+﻿namespace Web.Auth
+{
+    public class UserSession
+    {
+        public string ClieNtCode { get; set; }
+        public string Mail { get; set; }
+        public string Nickname { get; set; }
+        public string Role { get; set; }
+
+    }
+}
